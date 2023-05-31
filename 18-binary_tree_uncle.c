8185b4d76006e9cc976ae0067b4454fe8fee644b
unkown007 <victormatusse00@gmail.com>
2023-05-31 19:55:55 +0200

    fixed

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,8 +12,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
-	if (node->parent->parent->left->left == node ||
-			node->parent->parent->left->right == node)
+	if (node->parent->parent->left &&
+			(node->parent->parent->left->left == node ||
+			node->parent->parent->left->right == node))
 		return (node->parent->parent->right);
 	else
 		return (node->parent->parent->left);
