261578e454821337b6f6c24e31eb4d119bad8bfa
unkown007 <victormatusse00@gmail.com>
2023-06-04 22:57:05 +0200

    fixed and updated

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -3,70 +3,23 @@
 #include "binary_trees.h"
 
 /**
- * getMax - get the maximum value of the binary tree
- *
- * @tree: pointer to the root node of the tree
- *
- * Return: maximum value, or 0 otherwise
- */
-int getMax(binary_tree_t *tree)
-{
-	int max_l, max_r, value;
-
-	if (tree == NULL)
-		return (INT_MIN);
-	max_l = getMax(tree->left);
-	max_r = getMax(tree->right);
-	if (max_l > max_r)
-		value = max_l;
-	else
-		value = max_r;
-	if (value < tree->n)
-		value = tree->n;
-	return (value);
-}
-
-/**
- * getMin - get the minimum value of the tree
- *
- * @tree: pointer to the root node of the tree
- *
- * Return: minimum value, 0 otherwise
- */
-int getMin(binary_tree_t *tree)
-{
-	int min_l, min_r, value;
-
-	if (tree == NULL)
-		return (INT_MAX);
-	min_l = getMin(tree->left);
-	min_r = getMin(tree->right);
-	if (min_l > min_r)
-		value = min_r;
-	else
-		value = min_l;
-	if (value > tree->n)
-		value = tree->n;
-	return (value);
-}
-
-/**
  * isBST - checks if the tree is a valid BST
  *
  * @tree: pointer to the root of the tree
+ * @min: smallest node visited
+ * @max: largest node visited
  *
  * Return: 1 if BST, 0 otherwise
  */
-int isBST(const binary_tree_t *tree)
+int isBST(const binary_tree_t *tree, int min, int max)
 {
-	if (tree == NULL)
-		return (1);
-	if (tree->left && getMax(tree->left) > tree->n)
-		return (0);
-	if (tree->right && getMin(tree->right) < tree->n)
-		return (0);
-	if (!isBST(tree->left) || !isBST(tree->right))
-		return (0);
+	if (tree != NULL)
+	{
+		if (tree->n < min || tree->n > max)
+			return (0);
+		return (isBST(tree->left, min, tree->n) &&
+				isBST(tree->right, tree->n, max));
+	}
 	return (1);
 }
 
@@ -116,5 +69,5 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 		return (0);
 	if (duplicated(tree))
 		return (0);
-	return (isBST(tree));
+	return (isBST(tree, INT_MIN, INT_MAX));
 }
---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * isBST - checks if a binary tree is a valid AVL Tree
+ *
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: 1 if the tree is a BST, 0 otherwise
+ */
+int isBST(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+}
+
+/**
+ * binary_tree_is_avl - checks if a binary tree is a valid AVL tree
+ *
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: 1 if treeis a valid AVL Tree, 0 otherwise
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+}
