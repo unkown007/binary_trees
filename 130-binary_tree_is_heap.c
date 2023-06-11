525dd953ec16f7d969005c3fcfc54067133f416f
unkown007 <victormatusse00@gmail.com>
2023-06-12 01:06:23 +0200

    added file

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -2,13 +2,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_heap - checks if a binary tree is a valid Max Binary Heap
+ * isheap - checks if a binary tree is a valid Max Binary Heap
  *
  * @tree: pointer to the root node of the tree
  *
  * Return: 1 if the tree is a valid MAX Binary Heap
  */
-int binary_tree_is_heap(const binary_tree_t *tree)
+int isheap(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (1);
@@ -18,6 +18,20 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 		return (0);
 	if (tree->right && tree->right->n > tree->n)
 		return (0);
-	return (binary_tree_is_heap(tree->left) &&
-			binary_tree_is_heap(tree->right));
+	return (isheap(tree->left) &&
+			isheap(tree->right));
+}
+
+/**
+ * binary_tree_is_heap - checks if a binary tree is a valid Max Binary Heap
+ *
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: 1 if the tree is a valid MAX Binary Heap
+ */
+int binary_tree_is_heap(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (isheap(tree));
 }
